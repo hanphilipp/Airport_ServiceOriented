f5e0333a9c7617b6dd67c47bf8f680d81b55eacc
Jonas <j.hasenauer98@gmail.com>
2019-01-10 23:15:06 +0100

    Wind direction

--- src/airport/Airport.java
+++ src/airport/Airport.java
@@ -1,10 +1,13 @@
 package airport;
 
+import misc.WindDirection;
+
 public class Airport {
     private String name;
     private Gate[] gates;
+    private WindDirection currentWindDirection;
 
-    public Airport(String name) {
+    public Airport(String name, WindDirection currentWindDirection) {
         this.name = name;
         gates = new Gate[10];
     }
---
+++ src/misc/WindDirection.java
@@ -0,0 +1,5 @@
+package misc;
+
+public enum WindDirection {
+    EastWest, WestEast,
+}
